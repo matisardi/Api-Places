a6c114b45078548c170f7324dd4a2be5facc85d8
matisardi <matisardi@hotmail.com>
2023-11-27 13:44:48 -0300

    Cambio proxy

--- indexWeb.js
+++ indexWeb.js
@@ -18,25 +18,25 @@
 
 const cargarFotos = async() => {
     try {
-        // const respuesta = await fetch(`https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=cities+argentina&key=${apiKey}`);
-        const respuesta = await fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJCULTGU3ZhJURJtH-S4Bwlt0&key=${apiKey}`);
+        const respuesta = await fetch(`https://corsproxy.io/?https://maps.googleapis.com/maps/api/place/textsearch/json?query=cities+argentina&key=${apiKey}`);
+        // const respuesta = await fetch(`https://corsproxy.io/?https://maps.googleapis.com/maps/api/place/details/json?placeid=ChIJCULTGU3ZhJURJtH-S4Bwlt0&key=${apiKey}`);
         console.log(respuesta);
         if (respuesta.status === 200) {
             const datos = await respuesta.json();
-            // console.log(datos.results);
-            console.log(datos.result);
+            console.log(datos.results);
+            // console.log(datos.result);
             let imagenes = "";
             let foto = datos.result;
-            // datos.results.forEach(foto => {
+            datos.results.forEach(foto => {
                 imagenes += `
                     <div class="lugar">
                         <img class="poster" src="https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=${foto.photos[0].photo_reference}&key=${apiKey}">
                         <h3 class="titulo">${foto.name}</h3>
                         <p>${foto.formatted_address}</p>
-                        <a href="${foto.website}">Website</a>
+                        <!-- <a href="${foto.website}">Website</a> -->
                     </div>
                 `;
-            // });
+            });
             document.getElementById("contenedor").innerHTML = imagenes;
         }
     } catch (error) {
